c737412e64d824ee44aaee3d2433c94bdcf24509
Dhairya <dhairyabhrambhatt@yahoo.com>
2022-10-07 18:50:41 -0400

    y

--- Dealership.java
+++ Dealership.java
@@ -16,40 +16,39 @@ public class Dealership {
         carList = new ArrayList<Car>();
     }
 
-    public void addCar(String manufacturer, int make, String model, double basePrice, CarType type)
+    public void addCar(String man, int make, String model, double basePrice, CarType type)
     {
-        Car C = new Car(manufacturer, make, model, basePrice, type);
+        Car C = new Car(man, make, model, basePrice, type);
         carList.add(C);
     }
 
-    public void showCars(String manufacturer) {
-        // Iterating using for loop
+    public void showCars(String man) {
         for (int i = 0; i < carList.size(); i++)
 
             // Printing and display the elements in ArrayList
-            if (carList.get(i).getMan().equalsIgnoreCase(manufacturer)) {
+            if (carList.get(i).getMan().equalsIgnoreCase(man)) {
                 System.out.print(carList.get(i));
             }
 
     }
 
-    public void showCars(String manufacturer, int make) {
+    public void showCars(String man, int make) {
         // Iterating using for loop
         for (int i = 0; i < carList.size(); i++)
 
             // Printing and display the elements in ArrayList
-            if (carList.get(i).getMan().equalsIgnoreCase(manufacturer) && carList.get(i).getMake() == make) {
+            if (carList.get(i).getMan().equalsIgnoreCase(man) && carList.get(i).getMake() == make) {
                 System.out.print(carList.get(i));
             }
 
     }
 
-    public void showCars(String manufacturer, int make, double basePrice) {
+    public void showCars(String man, int make, double basePrice) {
         // Iterating using for loop
         for (int i = 0; i < carList.size(); i++)
 
             // Printing and display the elements in ArrayList
-            if (carList.get(i).getMan().equalsIgnoreCase(manufacturer) &&
+            if (carList.get(i).getMan().equalsIgnoreCase(man) &&
                     carList.get(i).getMake() == make && carList.get(i).getPrice() <= basePrice) {
                 System.out.print(carList.get(i));
             }
